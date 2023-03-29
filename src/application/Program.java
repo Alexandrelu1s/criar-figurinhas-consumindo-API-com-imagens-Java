aa8aa36a480d92f0ab73f29c3be332c87d07a79b
Alexandrelu1s <alexandrelu1s.ptn@gmail.com>
2023-03-29 18:13:11 -0300

    Desafio 02 - mostrar a nota do filme como estrelinhas, decorar o terminal com cores, negrito e itálico usando códigos ANSI

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,18 +12,15 @@ public class Program {
 
 	public static void main(String[] args) throws Exception {
 
-		// fazer uma conexão HTTP e buscar as séries mais populares
+		// fazer uma conexão HTTP e buscar os filmes mais populares
 
-		// String url =
-		// "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+		// String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
 
-		// String url =
-		// "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
+		// String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopTVs.json";
 
-		// String url =
-		// "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
+		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularMovies.json";
 
-		String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
+		// String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/MostPopularTVs.json";
 
 		URI endereco = URI.create(url);
 		HttpClient client = HttpClient.newHttpClient();
@@ -40,10 +37,15 @@ public class Program {
 
 		for (Map<String, String> filme : listaDeFilmes) {
 
-			System.out.println(filme.get("title"));
-			System.out.println(filme.get("image"));
-			System.out.println(filme.get("imDbRating"));
-			System.out.println();
+			System.out.println("\u001b[1mTitulo:\u001b[0m " + "\u001b[3m" + filme.get("title") + "\u001b[0m");
+            System.out.println("\u001b[1mURL imagem:\u001b[0m " + "\u001b[3m" + filme.get("image") + "\u001b[0m");
+            System.out.print("\u001b[1mClassificação:\u001b[0m ");
+            double classificacao = Double.parseDouble(filme.get("imDbRating"));
+            int numEstrelinhas = (int) classificacao;
+            for(int n = 1; n <= numEstrelinhas; n++) {
+            	System.out.print("\u001b[33m★\u001b[0m");
+            }
+            System.out.println("\n");
 
 		}
 
